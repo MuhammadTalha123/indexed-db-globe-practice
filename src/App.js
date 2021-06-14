e8ab580c6c163b68c8985786bf4ce16f5b107a49
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-06-14 23:44:53 +0500

    update inputs and handle values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { sha256 } from "js-sha256";
+
 
 function App() {
+
+  const [startValue, setStartValue] = useState(0);
+  const [endValue, setEndValue] = useState(0);
+
+  const handleStartValue = (e) => {
+      setStartValue(parseInt(e.target.value));
+  }
+
+  const handleEndValue = (e) => {
+    setEndValue(parseInt(e.target.value));
+}
+
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <h1>Blob Testing</h1>
+      <label>Start value</label>
+      <input type="number" value={startValue} onChange={handleStartValue} /> 
+      <label>End value</label>
+      <input type="number" value={endValue} onChange={handleEndValue} />
     </div>
   );
 }
--- src/IDB.js
+++ src/IDB.js
@@ -1,10 +1,10 @@
 import { openDB } from "idb";
 
-export const initializeDB = async (dbName) => {
-  const db = await openDB(dbName, 1, {
+export const initializeDB = async (dbName, storeName, version) => {
+  const db = await openDB(dbName, version, {
     upgrade(db) {
       // Create a store of objects
-      db.createObjectStore("data");
+      db.createObjectStore(storeName);
       // Create an index on the 'date' property of the objects.
       // storeRow.createIndex("number", "number");
     },
